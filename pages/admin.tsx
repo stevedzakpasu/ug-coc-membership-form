999b7a2037a8f27145a0d56e2b7eb15d4d5a95dc
Stephen Dzakpasu <steve.dzakpasu@gmail.com>
2023-01-23 12:34:24 +0000

    added protection to admin routes

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -7,6 +7,39 @@ function Admin() {
   const [membersData, setMembersData] = useState([]);
 
   useEffect(() => {
+    const Login = async () => {
+      if (typeof window !== "undefined") {
+        const token = localStorage.getItem("token");
+
+        const headers = {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          Authorization: `Bearer ${token}`,
+        };
+
+        await axios
+          .get("https://ug-attendance-app.herokuapp.com/users/me", {
+            headers: headers,
+          })
+          .then(async (res) => {
+            if (res.status == 200) {
+              if (!res.data.is_admin) {
+                router.push("/dashboard");
+              }
+            }
+          })
+          .catch(() => {
+            localStorage.setItem("isLoggedIn", "no");
+            alert("Your session has expired, kindly log in again");
+            router.push("/login");
+          });
+      }
+    };
+
+    Login();
+  }, []);
+
+  useEffect(() => {
     async function fetchData() {
       const token = localStorage.getItem("token");
       const headers = {
--- pages/admin/events.tsx
+++ pages/admin/events.tsx
@@ -1,5 +1,39 @@
-import React from "react";
+import axios from "axios";
+import router from "next/router";
+import React, { useEffect } from "react";
 
 export default function Events() {
+  useEffect(() => {
+    const Login = async () => {
+      if (typeof window !== "undefined") {
+        const token = localStorage.getItem("token");
+
+        const headers = {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          Authorization: `Bearer ${token}`,
+        };
+
+        await axios
+          .get("https://ug-attendance-app.herokuapp.com/users/me", {
+            headers: headers,
+          })
+          .then(async (res) => {
+            if (res.status == 200) {
+              if (!res.data.is_admin) {
+                router.push("/dashboard");
+              }
+            }
+          })
+          .catch(() => {
+            localStorage.setItem("isLoggedIn", "no");
+            alert("Your session has expired, kindly log in again");
+            router.push("/login");
+          });
+      }
+    };
+
+    Login();
+  }, []);
   return <div>Events Info</div>;
 }
