0a5b10cc0036cda5f957f09b52e4bccec2cd58a9
Stephen Dzakpasu <steve.dzakpasu@gmail.com>
2023-01-24 13:29:50 +0000

    added events page on the admin dashboard

--- pages/admin/events.tsx
+++ pages/admin/events.tsx
@@ -5,6 +5,20 @@ import React, { useEffect, useState } from "react";
 
 export default function Events() {
   const [isAdmin, setIsAdmin] = useState(false);
+  const [detail, setDetail] = useState({});
+  const [tableVisible, setTableVisible] = useState(true);
+  const [detailsVisible, setDetailsVisible] = useState(false);
+  const [eventsData, setEventsData] = useState([
+    {
+      name: null,
+      id: null,
+      semester: null,
+      category: null,
+      members_attended: [],
+      created_on: null,
+    },
+  ]);
+
   useEffect(() => {
     const Login = async () => {
       if (typeof window !== "undefined") {
@@ -39,35 +53,164 @@ export default function Events() {
 
     Login();
   }, [isAdmin]);
+  useEffect(() => {
+    async function fetchData() {
+      const token = localStorage.getItem("token");
+      const headers = {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        Authorization: `Bearer ${token}`,
+      };
+
+      await axios
+        .get(
+          "https://ug-attendance-app.herokuapp.com/api/events/?offset=0&limit=1000",
+          { headers: headers }
+        )
+        .then((res) => {
+          setEventsData(res.data);
+        });
+    }
+    fetchData();
+  }, []);
+
+  const EventData = eventsData.map((info) => {
+    return (
+      <tr key={info.id}>
+        <td
+          scope="row"
+          className="px-6 py-3 text-center text-xs font-medium text-gray-500 tracking-wider"
+        >
+          {info.id}
+        </td>{" "}
+        <td
+          scope="row"
+          className="px-6 py-3 text-center text-xs font-medium text-gray-500 tracking-wider"
+        >
+          {info.name}
+        </td>{" "}
+        <td
+          scope="row"
+          className="px-6 py-3 text-center text-xs font-medium text-gray-500 tracking-wider"
+        >
+          {info.category}
+        </td>{" "}
+        <td
+          scope="row"
+          className="px-6 py-3 text-center text-xs font-medium text-gray-500 tracking-wider"
+        >
+          {info.semester}
+        </td>{" "}
+        <td
+          scope="row"
+          className="px-6 py-3 text-center text-xs font-medium text-gray-500 tracking-wider"
+        >
+          {info.created_on}
+        </td>{" "}
+        <td
+          scope="row"
+          className="px-6 py-3 text-center text-xs font-medium text-gray-500 tracking-wider"
+        >
+          {info.members_attended.length}
+        </td>{" "}
+        <td
+          scope="row"
+          className=" text-center text-xs font-medium text-gray-500 tracking-wider"
+          onClick={() => {
+            setDetail(info);
+            setTableVisible(false);
+            setDetailsVisible(true);
+          }}
+        >
+          More Info
+        </td>
+      </tr>
+    );
+  });
+
+  const EventsDetails = (props: any) => {
+    return (
+      <div className="mx-10">
+        <div className="w-full flex-col flex mt-7">
+          <span>
+            <p
+              className="cursor-pointer"
+              onClick={() => {
+                setDetailsVisible(false);
+                setTableVisible(true);
+                setDetail({});
+              }}
+            >
+              Back
+            </p>
+          </span>
+          <h1 className="text-lg font-medium text-gray-500">
+            First Name: {props.info.name}
+          </h1>
+        </div>
+      </div>
+    );
+  };
+
   return (
-    <div className="flex items-center justify-center h-screen w-full flex-col bg-blue-200 ">
-      <Head>
-        <title>Admin Dashboard</title>
-      </Head>
+    <div className="m-6">
+      {detailsVisible && <EventsDetails info={detail} />}
 
       {isAdmin ? (
         <>
-          <h1 className="font-semibold text-2xl font-serif my-10">
-            Welcome Admin!{" "}
-          </h1>
-          <button>Members Information</button>
-          <button>Events Information</button>
-          <button>Stat Information</button>
-
-          <button
-            onClick={() => {
-              router.push("/login");
-              localStorage.clear();
-            }}
-            className="bg-blue-300"
-          >
-            Logout
-          </button>
+          {tableVisible && (
+            <>
+              <p>Number of events = {eventsData.length}</p>
+              <table className="min-w-full divide-y h-10 divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      EVENT ID
+                    </th>{" "}
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      EVENT NAME
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      CATEGORY
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      SEMESTER
+                    </th>{" "}
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      CREATED ON
+                    </th>{" "}
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      MEMBERS ATTENDED
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {EventData}
+                </tbody>
+              </table>
+            </>
+          )}
         </>
       ) : (
-        <>
-          <p>You do not have rights to see this page</p>
-        </>
+        <p> </p>
       )}
     </div>
   );
